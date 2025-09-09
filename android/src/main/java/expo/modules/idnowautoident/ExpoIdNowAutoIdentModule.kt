a6caf90a7b26fc3b5bbc741c2de1899738460d44
Haginus <hagiandrei.ah@gmail.com>
2025-09-10 01:57:12 +0300

    add android lib code

--- android/src/main/java/expo/modules/idnowautoident/ExpoIdNowAutoIdentModule.kt
+++ android/src/main/java/expo/modules/idnowautoident/ExpoIdNowAutoIdentModule.kt
@@ -1,32 +1,33 @@
 package expo.modules.idnowautoident
 
-import expo.modules.kotlin.modules.Module
-import expo.modules.kotlin.modules.ModuleDefinition
-import java.net.URL
 import android.content.Context
 import android.content.Intent
-import de.idnow.core.IDnowConfig
-import de.idnow.core.IDnowSDK
+import expo.modules.kotlin.Promise
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoIdNowAutoIdentModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoIdNowAutoIdent')` in JavaScript.
     Name("ExpoIdNowAutoIdent")
 
-    Function("startIdentification") { token: String ->
-      val context: Context? = appContext.reactContext?.applicationContext
-      val intent = Intent(context, IDNowStartActivity::class.java)
-      intent.putExtra("token", token)
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      context?.startActivity(intent)
-      return@Function "system"
+    // Async function that returns a promise to JS
+    AsyncFunction("startIdentification") { token: String, language: String?, promise: Promise ->
+      val context: Context =
+              appContext.reactContext?.applicationContext
+                      ?: throw Exception("Context not available")
 
+      // Store the promise in the Activity companion object
+      IDNowActivity.currentPromise = promise
+
+      val intent =
+              Intent(context, IDNowActivity::class.java).apply {
+                putExtra("token", token)
+                putExtra("language", language ?: "en")
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+              }
+
+      context.startActivity(intent)
     }
-
   }
 }
---
+++ android/src/main/java/expo/modules/idnowautoident/IDNowStartActivity.kt
@@ -0,0 +1,43 @@
+package expo.modules.idnowautoident
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import de.idnow.core.IDnowConfig
+import de.idnow.core.IDnowResult
+import de.idnow.core.IDnowSDK
+import expo.modules.kotlin.Promise
+
+class IDNowActivity : Activity(), IDnowSDK.IDnowResultListener {
+
+  companion object {
+    var currentPromise: Promise? = null
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val token: String = intent.getStringExtra("token") ?: ""
+    val language: String = intent.getStringExtra("language") ?: "en"
+
+    val idnowConfig = IDnowConfig.Builder.getInstance().withLanguage(language).build()
+
+    val sdk = IDnowSDK.getInstance()
+    sdk.initialize(this, idnowConfig)
+    sdk.startIdent(token.uppercase(), this)
+  }
+
+  override fun onIdentResult(result: IDnowResult) {
+    val mapResult =
+      when (result.resultType) {
+        IDnowResult.ResultType.FINISHED -> mapOf("type" to "finished")
+        IDnowResult.ResultType.CANCELLED -> mapOf("type" to "canceled")
+        IDnowResult.ResultType.ERROR -> mapOf("type" to "error", "errorCode" to result.statusCode)
+      }
+
+    currentPromise?.resolve(mapResult)
+    currentPromise = null
+
+    finish()
+  }
+}
