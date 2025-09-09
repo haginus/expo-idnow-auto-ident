d61e16a433e53d5c2b70d668509ef631a57db2fd
Haginus <hagiandrei.ah@gmail.com>
2025-09-10 01:57:22 +0300

    update example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import ExpoIdNowAutoIdent from 'expo-idnow-auto-ident';
 import React from 'react';
-import { Alert, Button, SafeAreaView, StyleSheet, Text, TextInput } from 'react-native';
+import { Alert, Button, KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [token, setToken] = React.useState('');
@@ -9,43 +9,54 @@ export default function App() {
     try {
       const result = await ExpoIdNowAutoIdent.startIdentification(token.toLocaleUpperCase(), 'en');
       Alert.alert('Identification Result', JSON.stringify(result));
+      console.log(result);
     } catch (error) {
       Alert.alert('Error', JSON.stringify(error));
+      console.error(error);
     }
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>Expo IDnow AutoIdent API Example</Text>
-      <TextInput
-        placeholder="Enter your token"
-        value={token}
-        onChangeText={setToken}
-        style={styles.input}
-      />
-      <Button
-        title="Start Identification"
-        onPress={startIdentification}
-      />
+    <SafeAreaView style={styles.safeArea}>
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={styles.header}>Enter your token</Text>
+          <TextInput
+            placeholder="xxx-xxxxx"
+            value={token}
+            onChangeText={setToken}
+            style={styles.input}
+          />
+          <Button
+            title="Start Identification"
+            onPress={startIdentification}
+          />
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+  },
   header: {
     fontSize: 30,
-    margin: 20,
+    textAlign: 'center',
+    marginBottom: 16,
   },
   input: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
-    marginHorizontal: 20,
-    marginBottom: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    marginBottom: 16,
   },
 });
